ce870ae6a288b6109efd783b93d76231db8a9265
hridoy65 <66057075+hridoy65@users.noreply.github.com>
2024-03-02 02:55:27 +0600

    Current Ramadan Highlight Added

---
+++ scripts/scripts.js
@@ -0,0 +1,14 @@
+function highlightCurrentRamadan() {
+    let currentDate = new Date().toLocaleDateString('en-GB', { timeZone: 'Asia/Dhaka' });
+    console.log(currentDate)
+    const element = document.getElementById(currentDate);
+    console.log(element)
+    if (element) {
+        element.classList.add('bg-green-600', 'text-white', 'font-extrabold');
+    }
+}
+
+window.onload = function () {
+    highlightCurrentRamadan();
+};
+
